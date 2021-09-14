06e9226e98f83805d717c91825877a3a704d9c18
Jonathan Burgos Vargas <jburgos@indulac.ec>
2021-09-14 16:21:43 -0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::get('user','App\Http\Controllers\UserController@getAuthenticatedUser');
+
+});
